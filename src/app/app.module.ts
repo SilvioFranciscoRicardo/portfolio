e56daf3ed609077da4f17f231207333fe35f2a7c
silvio francisco <silviofranciscoricardo@gmail.com>
2023-08-20 18:30:22 -0300

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PageHomeComponent } from './pages/page-home/page-home.component';
+import { HeaderComponent } from './components/header/header.component';
+import {MatIconModule} from  '@angular/material/icon' ;
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageHomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
